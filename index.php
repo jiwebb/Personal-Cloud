57f76cbbdb3b545cb710e5552e41c2c29d872fdd
Jimmy Webb <107872883+jiwebb@users.noreply.github.com>
2022-07-18 10:23:55 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title>Personal Cloud</title>
+</head>
+
+<body>
+<TABLE WIDTH="450" BORDER="0" CELLPADDING="2" ALIGN="center">
+<TR>
+	<TD ALIGN="center"><IMG SRC="assets/bullet_ball_green.png" WIDTH=150 HEIGHT=150 ALT="" BORDER="0"></TD>
+</TR>
+<TR ALIGN="center">
+    <TD><h1>Personal Cloud</h1></TD>
+</TR>
+<TR>
+    <TD HEIGHT="150" ALIGN="center" VALIGN="bottom">jiwebb@deloitte.com</TD>
+</TR>
+<TR>
+    <TD ALIGN="center"><img src="assets/name.jpg" width="135" height="28"></TD>
+</TR>
+</TABLE>
+
+</body>
+</html>
